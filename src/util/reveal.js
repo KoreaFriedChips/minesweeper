c35726ccdec4ade125a1b20931d449f661ea068b
Alex Xu <alex.xu.205@gmail.com>
2023-07-13 16:43:01 -0400

    left click on 0 block doesn't reveal itself

--- src/util/reveal.js
+++ src/util/reveal.js
@@ -10,6 +10,11 @@ export const revealed = (arr, x, y, newNonMinesCount, flag, row, col) => {
 
         for (let i = -1; i <= 1; i++) {
             for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0 && arr[x][y].value === 0) {
+                    arr[x][y].revealed = true;
+                    newNonMinesCount--;
+                    continue;
+                }
                 if (x + i >= row || x + i < 0 || y + j >= col || y + j < 0 || (i === 0 && j === 0))
                     continue;
                 if (arr[x + i][y + j].value !== "X" && arr[x + i][y + j].revealed === false) {
